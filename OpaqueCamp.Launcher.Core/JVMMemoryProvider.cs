6141bf8d45fc31a52d90813dd462142fc96e7742
jeffors <register.uzik@yandex.ru>
2022-11-08 16:10:16 +0500

    Crossplatform AutoMaxMemory()

--- OpaqueCamp.Launcher.Core/JVMMemoryProvider.cs
+++ OpaqueCamp.Launcher.Core/JVMMemoryProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Management;
+﻿using System.Diagnostics;
+using System.Management;
 
 namespace OpaqueCamp.Launcher.Core
 {
@@ -8,14 +9,41 @@ namespace OpaqueCamp.Launcher.Core
         public int MaxMemoryAllocation { get; set; } = 4096;
 
 
-        // Fuck this WMI shit. Are there other ways?
+        
         public void AutoMaxMemory()
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
-
-            foreach (var mobject in searcher.Get())
+            // WMI
+            if (OperatingSystem.IsWindows())
             {
-                MaxMemoryAllocation = (int)(Convert.ToInt64(mobject["TotalPhysicalMemory"]) / (1024*1024) / 2);
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
+
+                foreach (var mobject in searcher.Get())
+                {
+                    MaxMemoryAllocation = (int)(Convert.ToInt64(mobject["TotalPhysicalMemory"]) / (1024 * 1024) / 2);
+                }
+            }
+
+
+            // Needs testing if the launcher will be cross-platform
+            if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
+            {
+                var output = "";
+
+                var info = new ProcessStartInfo("free -m");
+                info.FileName = "/bin/bash";
+                info.Arguments = "-c \"free -m\"";
+                info.RedirectStandardOutput = true;
+
+                using(var process = Process.Start(info))
+                {
+                    output = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(output);
+                }
+
+                var lines = output.Split("\n");
+                var memory = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                MaxMemoryAllocation = (int)(Int64.Parse(memory[1]) / (1024 * 1024) / 2);
             }
         }
     }
