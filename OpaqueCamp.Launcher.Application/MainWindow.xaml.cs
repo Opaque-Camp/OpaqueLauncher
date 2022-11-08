f2c6110f5a31c5f8586e673e8c7053c77952418f
jeffors <register.uzik@yandex.ru>
2022-11-08 19:47:44 +0500

    Add controls for choose RAM and redownload the client

--- OpaqueCamp.Launcher.Application/MainWindow.xaml.cs
+++ OpaqueCamp.Launcher.Application/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using OpaqueCamp.Launcher.Core;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -33,5 +34,11 @@ public partial class MainWindow : Window
     {
         var debugWindow = new DebugWindow();
         debugWindow.Show();
+    }
+
+    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+    {
+        Regex regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
     }
 }
