6eac5622684ccc93ac7e60c0bda7886f745ad2b0
javabird25 <shockck84@gmail.com>
2022-11-09 19:22:11 +0500

    ISystemMemoryDetector implementations

---
+++ OpaqueCamp.Launcher.Infrastructure/UnixSystemMemoryDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using OpaqueCamp.Launcher.Core;
+
+namespace OpaqueCamp.Launcher.Infrastructure;
+
+public sealed class UnixSystemMemoryDetector : ISystemMemoryDetector
+{
+    public int GetSystemMemoryMegabytes()
+    {
+        string output;
+
+        var info = new ProcessStartInfo("free -m")
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"free -m\"",
+            RedirectStandardOutput = true
+        };
+
+        using (var process = Process.Start(info))
+        {
+            output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+        }
+
+        var lines = output.Split("\n");
+        var memory = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        return (int)(Int64.Parse(memory[1]) / (1024 * 1024) / 2);
+    }
+}
---
+++ OpaqueCamp.Launcher.Infrastructure/WindowsSystemMemoryDetector.cs
@@ -0,0 +1,15 @@
+﻿using System.Management;
+using System.Runtime.Versioning;
+using OpaqueCamp.Launcher.Core;
+
+namespace OpaqueCamp.Launcher.Infrastructure;
+
+[SupportedOSPlatform("windows")]
+public sealed class WindowsSystemMemoryDetector : ISystemMemoryDetector
+{
+    public int GetSystemMemoryMegabytes()
+    {
+        var searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
+        return (int)(Convert.ToInt64(searcher.Get().GetEnumerator().Current["TotalPhysicalMemory"]) / (1024 * 1024));
+    }
+}
