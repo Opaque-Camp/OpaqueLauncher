9af97439607af7335a286aa749bd2b5c79053be7
javabird25 <shockck84@gmail.com>
2022-11-09 19:25:37 +0500

    Implement JvmMemorySettingsRepository

---
+++ OpaqueCamp.Launcher.Application/JvmMemorySettingsRepository.cs
@@ -0,0 +1,20 @@
+﻿using OpaqueCamp.Launcher.Core;
+
+namespace OpaqueCamp.Launcher.Application;
+
+public sealed class JvmMemorySettingsRepository : IJvmMemorySettingsRepository
+{
+    public int GetInitialMemoryAllocationMegabytes() => Properties.Settings.Default.JvmInitialMemoryAllocationMegabytes;
+
+    public void SetInitialMemoryAllocationMegabytes(int value)
+    {
+        Properties.Settings.Default.JvmInitialMemoryAllocationMegabytes = value;
+    }
+
+    public int GetMaxMemoryAllocationMegabytes() => Properties.Settings.Default.JvmMaxMemoryAllocationMegabytes;
+
+    public void SetMaxMemoryAllocationMegabytes(int value)
+    {
+        Properties.Settings.Default.JvmMaxMemoryAllocationMegabytes = value;
+    }
+}
