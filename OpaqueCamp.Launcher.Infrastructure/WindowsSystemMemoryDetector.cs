85849e0c2e148d1592b823f6d8bf25e89bd88bc8
javabird25 <shockck84@gmail.com>
2022-11-09 19:30:29 +0500

    Fix WindowsSystemMemoryDetector

--- OpaqueCamp.Launcher.Infrastructure/WindowsSystemMemoryDetector.cs
+++ OpaqueCamp.Launcher.Infrastructure/WindowsSystemMemoryDetector.cs
@@ -10,6 +10,8 @@ public sealed class WindowsSystemMemoryDetector : ISystemMemoryDetector
     public int GetSystemMemoryMegabytes()
     {
         var searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
-        return (int)(Convert.ToInt64(searcher.Get().GetEnumerator().Current["TotalPhysicalMemory"]) / (1024 * 1024));
+        var searcherEnumerator = searcher.Get().GetEnumerator();
+        searcherEnumerator.MoveNext();
+        return (int)(Convert.ToInt64(searcherEnumerator.Current["TotalPhysicalMemory"]) / (1024 * 1024));
     }
 }
