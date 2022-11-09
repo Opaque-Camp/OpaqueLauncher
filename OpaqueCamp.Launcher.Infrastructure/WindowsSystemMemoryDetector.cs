bad4d1834b846d75aac7fda8ad43ba0c420f643c
javabird25 <shockck84@gmail.com>
2022-11-09 20:00:35 +0500

    Add Windows RAM query via WMIC for Windows 7

--- OpaqueCamp.Launcher.Infrastructure/WindowsSystemMemoryDetector.cs
+++ OpaqueCamp.Launcher.Infrastructure/WindowsSystemMemoryDetector.cs
@@ -1,4 +1,5 @@
-﻿using System.Management;
+﻿using System.Diagnostics;
+using System.Management;
 using System.Runtime.Versioning;
 using OpaqueCamp.Launcher.Core;
 
@@ -9,9 +10,38 @@ public sealed class WindowsSystemMemoryDetector : ISystemMemoryDetector
 {
     public int GetSystemMemoryMegabytes()
     {
+        return IsWindows8OrNewer() ? GetMemoryForWindows8AndOlder() : GetMemoryForWindows7();
+    }
+
+    private bool IsWindows8OrNewer() => OperatingSystem.IsWindowsVersionAtLeast(6, 3);
+
+    private int GetMemoryForWindows8AndOlder()
+    {
         var searcher = new ManagementObjectSearcher("select * from Win32_ComputerSystem");
         var searcherEnumerator = searcher.Get().GetEnumerator();
         searcherEnumerator.MoveNext();
         return (int)(Convert.ToInt64(searcherEnumerator.Current["TotalPhysicalMemory"]) / (1024 * 1024));
+    }
+
+    private int GetMemoryForWindows7()
+    {
+        string output;
+
+        var info = new ProcessStartInfo
+        {
+            FileName = "wmic",
+            Arguments = "OS get FreePhysicalMemory,TotalVisibleMemorySize /Value",
+            RedirectStandardOutput = true
+        };
+
+        using (var process = Process.Start(info))
+        {
+            output = process.StandardOutput.ReadToEnd();
+        }
+
+        var lines = output.Trim().Split("\n");
+        var memory = lines[1].Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+        return (int)(Math.Round(Double.Parse(memory[1])) / 1024);
     }
 }
