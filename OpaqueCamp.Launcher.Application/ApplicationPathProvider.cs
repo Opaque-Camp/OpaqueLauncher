b26dbae17f858746ff3cb17022ca8f59443c010b
javabird25 <shockck84@gmail.com>
2022-11-12 11:36:56 +0500

    Implement IApplicationPathProvider

---
+++ OpaqueCamp.Launcher.Application/ApplicationPathProvider.cs
@@ -0,0 +1,8 @@
+﻿using OpaqueCamp.Launcher.Core;
+
+namespace OpaqueCamp.Launcher.Application;
+
+public sealed class ApplicationPathProvider : IApplicationPathProvider
+{
+    public string ApplicationPath => System.AppDomain.CurrentDomain.BaseDirectory;
+}
