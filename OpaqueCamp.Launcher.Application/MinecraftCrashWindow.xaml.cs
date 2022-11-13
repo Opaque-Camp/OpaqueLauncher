5be9e0f955dd685d8855713c52f23a5800db55fe
jeffors <register.uzik@yandex.ru>
2022-11-13 18:51:41 +0500

    Rewrite CrashWindow to AdonisUI contriks

--- OpaqueCamp.Launcher.Application/MinecraftCrashWindow.xaml.cs
+++ OpaqueCamp.Launcher.Application/MinecraftCrashWindow.xaml.cs
@@ -1,9 +1,10 @@
-﻿using OpaqueCamp.Launcher.Core;
+﻿using AdonisUI.Controls;
+using OpaqueCamp.Launcher.Core;
 using System.Windows;
 
 namespace OpaqueCamp.Launcher.Application;
 
-public partial class MinecraftCrashWindow : Window
+public partial class MinecraftCrashWindow : AdonisWindow
 {
     public MinecraftCrashWindow(MinecraftCrashLogs crashLogs)
     {
