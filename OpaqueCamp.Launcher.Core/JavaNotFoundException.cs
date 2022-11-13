c8fb1642d285f88751ff5cb3d705884108096834
jeffors <register.uzik@yandex.ru>
2022-11-13 22:40:13 +0500

    Grammar fix

--- OpaqueCamp.Launcher.Core/JavaNotFoundException.cs
+++ OpaqueCamp.Launcher.Core/JavaNotFoundException.cs
@@ -3,7 +3,7 @@
 public sealed class JavaNotFoundException : UserFriendlyException
 {
     public JavaNotFoundException() : base(
-        "Не удалось найти установленную Java на вашем компьютере. Установите Java, либо, если вы уверенны, что она уже установлена, попробуйте переустановить её.",
+        "Не удалось найти установленную Java на вашем компьютере. Установите Java, либо, если вы уверены, что она уже установлена, попробуйте переустановить её.",
         "javaw.exe was not found because JAVA_HOME is not set or set incorrectly and javaw.exe is missing in PATH.")
     {
     }
