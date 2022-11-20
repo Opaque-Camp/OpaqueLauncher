a44882b8ed6a982cf89047668782c50d76840e36
javabird25 <shockck84@gmail.com>
2022-11-20 16:16:00 +0500

    Fix crash due to extracting mods again

--- OpaqueCamp.Launcher.Infrastructure/BundledZipModsInstaller.cs
+++ OpaqueCamp.Launcher.Infrastructure/BundledZipModsInstaller.cs
@@ -6,18 +6,43 @@ namespace OpaqueCamp.Launcher.Infrastructure;
 public sealed class BundledZipModsInstaller : IModsInstaller
 {
     private readonly IMinecraftFilesDirProvider _minecraftFilesDirProvider;
+    private readonly IFileSystem _fileSystem;
     private readonly IModZipBundlePathProvider _modZipBundlePathProvider;
 
     public BundledZipModsInstaller(IModZipBundlePathProvider modZipBundlePathProvider,
-        IMinecraftFilesDirProvider minecraftFilesDirProvider)
+        IMinecraftFilesDirProvider minecraftFilesDirProvider, IFileSystem fileSystem)
     {
         _modZipBundlePathProvider = modZipBundlePathProvider;
         _minecraftFilesDirProvider = minecraftFilesDirProvider;
+        _fileSystem = fileSystem;
     }
 
     public async Task InstallModsAsync()
     {
-        await Task.Run(() => ZipFile.ExtractToDirectory(_modZipBundlePathProvider.ModZipBundlePath,
-            _minecraftFilesDirProvider.ModsPath));
+        if (!IsModFolderEmpty())
+        {
+            return;
+        }
+
+        await Task.Run(() =>
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(_modZipBundlePathProvider.ModZipBundlePath,
+                    _minecraftFilesDirProvider.ModsPath);
+            }
+            catch (IOException e)
+            {
+                if (!e.Message.Contains("already exists"))
+                {
+                    throw;
+                }
+            }
+        });
+    }
+
+    private bool IsModFolderEmpty()
+    {
+        return _fileSystem.IsDirectoryEmpty(_minecraftFilesDirProvider.ModsPath);
     }
 }
