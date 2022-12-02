d1f680634d2f063a7453013f0602421cb2b16d67
javabird25 <shockck84@gmail.com>
2022-12-02 11:20:03 +0500

    Add MicrosoftAccountEditor

---
+++ OpaqueCamp.Launcher.Application/MicrosoftAccountEditor.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using CmlLib.Core.Auth.Microsoft.UI.Wpf;
+
+namespace OpaqueCamp.Launcher.Application;
+
+public partial class MicrosoftAccountEditor
+{
+    public MicrosoftAccountEditor()
+    {
+        InitializeComponent();
+    }
+
+    private async void LogIn(object sender, RoutedEventArgs e)
+    {
+        var loginWindow = new MicrosoftLoginWindow();
+        var session = await loginWindow.ShowLoginDialog();
+        MessageBox.Show("Login success : " + session.Username);
+    }
+}
